c968ab5096c09b7a9d50b3c4359c030ebeebbcf9
Darcy Voutt <darcyvoutt@gmail.com>
2014-03-05 02:03:32 -0500

    arranging less and js scripts

--- header.php
+++ header.php
@@ -6,10 +6,22 @@
   <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
   
   <title>Exclaim HTML Mockup</title>
-  
-  <link rel="stylesheet" href="">
 
-  <script></script>
+  <!-- Less -->
+  <script type="text/javascript">
+    less = {
+        env: "development",
+        async: false,         // load imports async
+        fileAsync: false,     // load imports async when in a page under
+        poll: 1000,           // when in watch mode, time in ms between polls
+        relativeUrls: false,  // whether to adjust url's to be relative
+      };
+  </script>
+  <link rel="stylesheet/less" href="assets/less/style.less" />
+
+  <!-- Scripts -->
+  <script src="assets/js/libraries/less-1.5.0.min.js"></script>
+  <script src="assets/js/libraries/jquery-1.11.0.js"></script>
   
 </head>
   
