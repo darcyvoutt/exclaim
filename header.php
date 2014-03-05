a0a611f2658bf1dee3c5bd424782ff3a37c0fd95
Darcy Voutt <darcyvoutt@gmail.com>
2014-03-04 19:52:50 -0500

    adding libraries to the project

--- header.php
+++ header.php
@@ -1,7 +1,18 @@
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>Exclaim HTML Mockup</title>
-  </head>
-  <body>
+<head>
+
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+  
+  <title>Exclaim HTML Mockup</title>
+  
+  <link rel="stylesheet" href="">
+
+  <script></script>
+  
+</head>
+  
+<body>
     
   
