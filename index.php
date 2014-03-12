5a1fa9cfbad7abbf427577ab88d2dc1b48027c74
Darcy Voutt <darcyvoutt@gmail.com>
2014-03-11 20:15:03 -0400

    changed streams to use flexbox

--- index.php
+++ index.php
@@ -3,26 +3,30 @@
 <div class="relatedArticles-headline"></div>
 
 <div class="content">
-  
-  <div class="stream" style="height: 400px;">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ultricies est id urna venenatis 
-    posuere vel eget metus. Fusce tincidunt nisl nisi, ut pulvinar elit ullamcorper nec. Quisque sagittis 
-    metus vitae sapien placerat, quis blandit sem accumsan. Duis in lacinia arcu. Etiam sodales dignissim 
-    sapien vel ultricies.</p>
-  </div>
 
-  <div class="stream" style="height: 500px;">
-     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ultricies est id urna venenatis 
-    posuere vel eget metus. Fusce tincidunt nisl nisi, ut pulvinar elit ullamcorper nec. Quisque sagittis 
-    metus vitae sapien placerat, quis blandit sem accumsan. Duis in lacinia arcu. Etiam sodales dignissim 
-    sapien vel ultricies.</p>
-  </div>
+  <div class="streams">
+    
+    <div class="stream" style="height: 400px;">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ultricies est id urna venenatis 
+      posuere vel eget metus. Fusce tincidunt nisl nisi, ut pulvinar elit ullamcorper nec. Quisque sagittis 
+      metus vitae sapien placerat, quis blandit sem accumsan. Duis in lacinia arcu. Etiam sodales dignissim 
+      sapien vel ultricies.</p>
+    </div>
 
-  <div class="stream" style="height: 350px;">
-     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ultricies est id urna venenatis 
-    posuere vel eget metus. Fusce tincidunt nisl nisi, ut pulvinar elit ullamcorper nec. Quisque sagittis 
-    metus vitae sapien placerat, quis blandit sem accumsan. Duis in lacinia arcu. Etiam sodales dignissim 
-    sapien vel ultricies.</p>
+    <div class="stream" style="height: 500px;">
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ultricies est id urna venenatis 
+      posuere vel eget metus. Fusce tincidunt nisl nisi, ut pulvinar elit ullamcorper nec. Quisque sagittis 
+      metus vitae sapien placerat, quis blandit sem accumsan. Duis in lacinia arcu. Etiam sodales dignissim 
+      sapien vel ultricies.</p>
+    </div>
+
+    <div class="stream" style="height: 350px;">
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ultricies est id urna venenatis 
+      posuere vel eget metus. Fusce tincidunt nisl nisi, ut pulvinar elit ullamcorper nec. Quisque sagittis 
+      metus vitae sapien placerat, quis blandit sem accumsan. Duis in lacinia arcu. Etiam sodales dignissim 
+      sapien vel ultricies.</p>
+    </div>
+
   </div>
 
   <script src="assets/js/streams.js"></script>
