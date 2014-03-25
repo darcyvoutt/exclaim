bc46907b43783fd5fb4c1b680af1404e138dabb7
Darcy Voutt <darcyvoutt@gmail.com>
2014-03-25 00:20:08 -0400

    updated the header section

--- header.php
+++ header.php
@@ -29,9 +29,35 @@
     </h1> <!-- // End of .logo -->
 
     <div class="navigation">
-      <div class="search"></div>
-      <div class="menu"></div>
-    </div>
+      <div class="navigation-search">
+        <div class="search">
+          <input type="text" />
+        </div>
+        <div class="social">
+          <ul>
+            <li><a href="#"><img src="http://bit.ly/1pum0Nr" alt="Social Link" /></a></li>
+            <li><a href="#"><img src="http://bit.ly/1pum0Nr" alt="Social Link" /></a></li>
+            <li><a href="#"><img src="http://bit.ly/1pum0Nr" alt="Social Link" /></a></li>
+            <li><a href="#"><img src="http://bit.ly/1pum0Nr" alt="Social Link" /></a></li>
+            <li><a href="#"><img src="http://bit.ly/1pum0Nr" alt="Social Link" /></a></li>
+          </ul>
+        </div>        
+      </div>
+      
+      <ul class="menu-dropdown">
+        <li class="menu-dropdown-list">
+          <a data-toggle="dropdown" href="#" class="dropdown-toggle">Music</a>
+          <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+            <li><a href="#">Videogames</a></li>
+            <li><a href="#">Film</a></li>
+            <li><a href="#">Art</a></li>
+          </ul>
+        </li>
+      </ul>
+
+      .
+
+    </div> <!-- // End of .navigation -->
 
   </div> <!-- // End of .header-content -->
 </div> <!-- // End of .header -->
