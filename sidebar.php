a683373b067304b552788999dbc77ede63e444ae
Darcy Voutt <darcyvoutt@gmail.com>
2014-03-30 17:20:24 -0400

    setup the widget list item

--- sidebar.php
+++ sidebar.php
@@ -10,7 +10,6 @@
     </div>
   </div>
 
-
   <!-- Standard Widget -->
   <div class="widget">
     <div class="widget-title">
@@ -25,6 +24,21 @@
   <a href="#" alt="Ads" class="ad-sidebar">
     <img src="assets/images/ad-sidebar.png" alt="Ad Test" />
   </a> <!-- // End of .ad-leaderboard -->
+
+  <!-- List Widget -->
+  <div class="widget-list">
+    <div class="widget-title">List Widget</div>
+    <div class="widget-content">
+      <ul class="articleList">
+        <li class="articleList-item">
+          <!-- List Item -->
+          <div class="articleList-item-category">Reviews</div>
+          <p>CONCERT: How to Dress Well / Forest Swords Virgin Mod Club, 
+          <span class="articleList-item-date">Toronto ON, March 29</span></p>
+        </li>
+      </ul>
+    </div>
+  </div>
 
 </div>
 
