f8a674e3f7a6362396726fd013a7b06765f99816
Darcy Voutt <darcyvoutt@gmail.com>
2014-03-30 16:58:50 -0400

    footer setup

--- footer.php
+++ footer.php
@@ -3,7 +3,29 @@
 
 <div class="footer">
   <div class="footer-content">
-    
+    <ul>
+      <li class="footer-content-emphasis">December 13 </li>
+      <li class="footer-content-category">News Features</li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+      <li class="footer-content-category">Reviews</li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+      <li class="footer-content-category">Contests</li>
+      <li><a href="#">Link</a></li>
+      <li class="footer-content-category">News Features</li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+      <li class="footer-content-category">Reviews</li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+      <li class="footer-content-category">Contests</li>
+      <li><a href="#">Link</a></li>
+    </ul>
   </div>
 </div>
 
