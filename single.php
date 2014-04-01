d0c5c15bedcaa9f419ecc770f310c26038fa476c
Darcy Voutt <darcyvoutt@gmail.com>
2014-04-01 08:59:15 -0400

    renamed synopsis to single.php and finished the styling

---
+++ single.php
@@ -0,0 +1,83 @@
+<?php include ('header.php') ?>
+
+<?php include('relatedArticles.php'); ?>
+
+<div class="content">
+
+  <div id="articleCarousel">
+    <?php include('carousel.php'); ?>
+  </div>
+
+  <?php include('filters.php'); ?>
+
+  <div class="streams">
+
+    <div class="stream-header is-active"><h2>Concert Listings</h2></div>
+
+  </div>
+
+  <div class="streams">
+    
+    <div class="stream">
+      <div class="stream-title">
+        <h3>News</h3>
+      </div>
+
+      <ul class="streamSingle">
+
+        <!-- List Item -->
+        <li class="streamSingle-item">          
+          <a href="#">
+            <img src="assets/images/stream2.jpg" alt="">
+            <h4 class="streamSingle-item-title">The Afghan Whigs / Crocodiles</h4> 
+            <span class="streamSingle-item-details">
+              Forest Swords Virgin Mod Club, Toronto ON, March 29
+            </span>
+            <p class="streamSingle-item-excerpt">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel augue ac quam placerat pharetra ac id enim. Fusce lectus diam, tempus pellentesque purus ut, laoreet elementum.
+            </p>
+          </a>
+        </li>
+
+        <!-- List Item -->
+        <li class="streamSingle-item">          
+          <a href="#">
+            <img src="assets/images/stream1.jpg" alt="">
+            <h4 class="streamSingle-item-title">The Afghan Whigs / Crocodiles</h4> 
+            <span class="streamSingle-item-details">
+              Forest Swords Virgin Mod Club, Toronto ON, March 29
+            </span>
+            <p class="streamSingle-item-excerpt">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel augue ac quam placerat pharetra ac id enim. Fusce lectus diam, tempus pellentesque purus ut, laoreet elementum.
+            </p>
+          </a>
+        </li>
+
+        <!-- List Item -->
+        <li class="streamSingle-item">          
+          <a href="#">
+            <img src="assets/images/stream2.jpg" alt="">
+            <h4 class="streamSingle-item-title">The Afghan Whigs / Crocodiles</h4> 
+            <span class="streamSingle-item-details">
+              Forest Swords Virgin Mod Club, Toronto ON, March 29
+            </span>
+            <p class="streamSingle-item-excerpt">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel augue ac quam placerat pharetra ac id enim. Fusce lectus diam, tempus pellentesque purus ut, laoreet elementum.
+            </p>
+          </a>
+        </li>
+
+      </ul>
+
+
+    </div>
+
+  </div>
+
+  <!-- <script src="assets/js/streams.js"></script> -->
+
+</div>
+
+<?php include ('sidebar.php') ?>
+
+<?php include ('footer.php') ?>
