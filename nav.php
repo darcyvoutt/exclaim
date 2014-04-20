40e19658749325c43e5a7cf3bdcb9135773fc3c2
Darcy Voutt <darcyvoutt@gmail.com>
2014-04-19 21:44:26 -0400

    updating navigation to work in mobile and desktop

--- nav.php
+++ nav.php
@@ -1,38 +1,40 @@
-<nav class="navbar" role="navigation">
-  <div class="container-fluid">
+<div class="mainNav">
+  <nav class="navbar" role="navigation">
+    <div class="container-fluid">
 
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" 
-              data-target="#bs-example-navbar-collapse-1">
-      </button>
-      <h1 class="navbar-logo">
-        <a class="navbar-brand" href="index.php">Exclaim!</a>
-      </h1>
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" 
+                data-target="#bs-example-navbar-collapse-1">
+        </button>
+        <h1 class="navbar-logo">
+          <a class="navbar-brand" href="index.php">Exclaim!</a>
+        </h1>
+      </div>
+
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+        <ul class="nav navbar-nav navbar-right">
+          <li class="navbar-link"><a href="index.php">Features</a></li>
+          <li class="navbar-link"><a href="index.php">Reviews</a></li>        
+          <li class="navbar-link"><a href="index.php">Streams</a></li>                
+          <li class="navbar-link"><a href="index.php">News</a></li>  
+          
+          
+          <li class="dropdown sections">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Music
+              <b class="caret"></b>
+            </a>
+            <ul class="dropdown-menu">
+              <li><a href="index.php">Videogames</a></li>
+              <li><a href="index.php">Film</a></li>
+              <li><a href="index.php">Art</a></li>
+              <li class="visible-mobile"><a href="index.php">Contests</a></li>        
+              <li class="visible-mobile"><a href="index.php">Contact</a></li> 
+            </ul>
+          </li>
+        </ul>
+
+      </div>
     </div>
-
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-      <ul class="nav navbar-nav navbar-right">
-        <li class="navbar-link"><a href="#">Features</a></li>
-        <li class="navbar-link"><a href="#">Reviews</a></li>        
-        <li class="navbar-link"><a href="#">Streams</a></li>                
-        <li class="navbar-link"><a href="#">News</a></li>  
-        
-        
-        <li class="dropdown sections">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Music
-            <b class="caret"></b>
-          </a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Videogames</a></li>
-            <li><a href="#">Film</a></li>
-            <li><a href="#">Art</a></li>
-            <li class="visible-mobile"><a href="#">Contests</a></li>        
-            <li class="visible-mobile"><a href="#">Contact</a></li> 
-          </ul>
-        </li>
-      </ul>
-
-    </div>
-  </div>
-</nav>
+  </nav>
+</div>
