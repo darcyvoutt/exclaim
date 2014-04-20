89bcc1612ce616c53b40f361bc05c22bb9dd3267
Darcy Voutt <darcyvoutt@gmail.com>
2014-04-19 22:26:15 -0400

    added search to mobile nav, added better .visible-mobile support and fixes along the way

--- nav.php
+++ nav.php
@@ -14,17 +14,20 @@
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
         <ul class="nav navbar-nav navbar-right">
+          <form role="search" class="visibile-mobile">
+            <input type="text" placeholder="Search Exclaim...">
+          </form>
+
           <li class="navbar-link"><a href="index.php">Features</a></li>
           <li class="navbar-link"><a href="index.php">Reviews</a></li>        
           <li class="navbar-link"><a href="index.php">Streams</a></li>                
           <li class="navbar-link"><a href="index.php">News</a></li>  
           
-          
           <li class="dropdown sections">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Music
               <b class="caret"></b>
             </a>
-            <ul class="dropdown-menu">
+            <ul class="dropdown-menu">              
               <li><a href="index.php">Videogames</a></li>
               <li><a href="index.php">Film</a></li>
               <li><a href="index.php">Art</a></li>
