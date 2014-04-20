332355f4cd09c10ba411666bb972bbe11c142649
Darcy Voutt <darcyvoutt@gmail.com>
2014-04-20 13:12:08 -0400

    updated streams for desktop and mobile

--- streamitem.php
+++ streamitem.php
@@ -5,12 +5,15 @@ to center the image. Therefore a new image does not need to be cropped.
 -->
 
 <div class="streamItem">
-  <a href="article.php"><img src="assets/images/stream2.jpg" alt="Image" />
+  <a href="article.php">
+  <img src="assets/images/stream2.jpg" alt="Image" />
+  
   <div class="streamItem-content">
-    <span class="streamItem-category">Video</span>
+    <div class="streamItem-category">Video</div>
     <span class="streamItem-title">Angel Haze</span>
     <span class="streamItem-subtitle">Drunk in Love</span>
-    <span class="streamItem-meta">(Beyonce cover) (live in-studio)</span>
+    <span class="streamItem-info">(Beyonce cover) (live in-studio)</span>
+    <div class="streamItem-meta">By Michael Rancic| Mar 28 2014</div>
   </div>
   </a>
 </div>
