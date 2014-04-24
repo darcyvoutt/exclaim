599470be210374da0518b1f55452ac7934713a2b
Darcy Voutt <darcyvoutt@gmail.com>
2014-04-24 08:38:17 -0400

    working on the search results

--- search.php
+++ search.php
@@ -68,7 +68,7 @@
 
     <!-- Festival -->
     <div class="widget">
-      <div class="widget-title">Festival</div>
+      <div class="widget-title">Festivals</div>
       <div class="widget-content">
         <ul class="widgetList">
           
@@ -92,13 +92,55 @@
               <p class="widgetList-item-excerpt">Festival Name Here</p>
             </a>
           </li>
+          
+        </ul>
+      </div>
+    </div>
+
+    <!-- Concerts -->
+    <div class="widget">
+      <div class="widget-title">Concerts</div>
+      <div class="widget-content">
+        <ul class="widgetList">
+          
+          <!-- List Item -->
+          <li class="widgetList-item">            
+            <a href="article.php">
+              <div class="widgetList-item-title">Festival Name Here</div>
+              <div class="widgetList-item-info">
+                Featuring: JFM | Ravi Naimphy | You'll Never Get Heaven | <b>Cancer Bats</b>
+              </div>
+            </a>            
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+
+    <!-- Keywords -->
+    <div class="widget">
+      <div class="widget-title">Concerts</div>
+      <div class="widget-content">
+        <ul class="widgetList">
+          
+          <!-- List Item -->
+          <li class="widgetList-item">              
+            <a href="article.php">              
+              <img src="assets/images/temp/stream1.jpg" alt="Cancer Bats"> 
+              <div class="widgetList-item-category">Pop</div>
+
+              <p class="widgetList-item-excerpt">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam arcu sapien, dictum eu sapien id, lacinia elementum sem. Cras fringilla neque tortor, sed molestie arcu dictum sed. Etiam diam arcu, luctus non pretium nec, congue ac enim. <span class="widgetList-item-more">Read More</span></p>
+            </a>            
+          </li>
 
           <li class="widgetList-more">
             <a href="article.php">See more albums</a>
           </li>
+          
         </ul>
       </div>
     </div>
+
 
     
   </div> <!-- End Search Results -->
